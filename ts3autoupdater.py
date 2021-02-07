8e94c78079520cdba5be028a13189201002394b8
Lassi Eloranta <lassi.eloranta@gmail.com>
2021-02-07 03:16:56 +0200

    Add some basic code documentation and fix some PEP8 problems

--- ts3autoupdater.py
+++ ts3autoupdater.py
@@ -4,15 +4,17 @@ import urllib.request
 from packaging import version as ver
 import time
 from datetime import datetime, timedelta
-from helperfunctions import get_current_version, check_for_updates, download_update, ts3_instance_management, install_update
+from helperfunctions import get_current_version, check_for_updates,\
+     download_update, ts3_instance_management, install_update
 
 old_version: str
 new_version: str
-download_url = "https://teamspeak.com/en/downloads/"
-dlreq = urllib.request.Request(download_url, headers={'User-Agent': 'Mozilla/5.0'})
+dl_url = "https://teamspeak.com/en/downloads/"
+dlreq = urllib.request.Request(dl_url, headers={'User-Agent': 'Mozilla/5.0'})
 
 
 def update_ts3_if_needed():
+    """Checks if there's an update for TS3 server and installs it if so"""
     old_version = get_current_version()
     new_version = check_for_updates(old_version, dlreq)
     if(ver.parse(new_version) > ver.parse(old_version)):
@@ -26,6 +28,7 @@ def update_ts3_if_needed():
 
 
 def restore_ts3_from_crash():
+    """Checks whether the TS3 has crashed and restarts it if so."""
     status = ts3_instance_management("status")
     if(status == "Up"):
         print("Server is running normally")
@@ -44,6 +47,8 @@ def main():
             print("Checking for server updates")
             update_ts3_if_needed
             last_checked = datetime.utcnow()
+        else:
+            print("Skipping server update check")
         print("Checking if server has crashed")
         restore_ts3_from_crash()
         print("sleeping for 10 minutes")
